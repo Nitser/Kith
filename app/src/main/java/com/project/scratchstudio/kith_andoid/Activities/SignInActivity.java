6c813e1e3c25a94d88006dbeb19b0bedd6d32ea3
Ксения Ницер <trif.nitser@gmail.com>
2019-02-11 05:57:11 +0300

    Sign-in Activity

--- app/src/main/java/com/project/scratchstudio/kith_andoid/Activities/SignInActivity.java
+++ app/src/main/java/com/project/scratchstudio/kith_andoid/Activities/SignInActivity.java
@@ -1,27 +1,19 @@
 package com.project.scratchstudio.kith_andoid.Activities;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.project.scratchstudio.kith_andoid.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
 
     public void signInButton(View view) {
-        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-        startActivity(intent);
-    }
-
-
-    public void registrationButton(View view) {
-
     }
 }
