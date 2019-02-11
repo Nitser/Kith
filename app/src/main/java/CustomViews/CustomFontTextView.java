f66ac73c8dba28010b661f4f63e3d2fcd6d34b0b
Ксения Ницер <trif.nitser@gmail.com>
2019-02-11 04:02:38 +0300

    CustomFontTextView

--- app/src/main/java/CustomViews/CustomFontTextView.java
+++ app/src/main/java/CustomViews/CustomFontTextView.java
@@ -1,4 +1,51 @@
 package CustomViews;
 
-public class CustomFontTextView {
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import java.util.Locale;
+
+public class CustomFontTextView extends android.support.v7.widget.AppCompatTextView {
+
+    private static final String sScheme =
+            "http://schemas.android.com/apk/res-auto";
+    private static final String sAttribute = "customFont";
+
+    static enum CustomFont {
+        JURA("fonts/JuraMedium.ttf"),
+        INTRO("fonts/Intro-Book.otf"),
+        INTRO_REGULAR("fonts/Intro-Regular.otf");
+
+        private final String fileName;
+
+        CustomFont(String fileName) {
+            this.fileName = fileName;
+        }
+
+        static CustomFont fromString(String fontName) {
+            return CustomFont.valueOf(fontName.toUpperCase(new Locale("ru", "RU")));
+        }
+
+        public Typeface asTypeface(Context context) {
+            return Typeface.createFromAsset(context.getAssets(), fileName);
+        }
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if (isInEditMode()) {
+            return;
+        } else {
+            final String fontName = attrs.getAttributeValue(sScheme, sAttribute);
+
+            if (fontName == null) {
+                throw new IllegalArgumentException("You must provide \"" + sAttribute + "\" for your text view");
+            } else {
+                final Typeface customTypeface = CustomFont.fromString(fontName).asTypeface(context);
+                setTypeface(customTypeface);
+            }
+        }
+    }
 }
