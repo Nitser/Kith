d943be54229ec544e731045d0bb35935e53f8cbb
Kseniya Nitser <trif.nitser@gmail.com>
2019-05-23 17:24:35 +0300

    include adapter to message fragment

--- app/src/main/java/com/project/scratchstudio/kith_andoid/Fragments/MessagesFragment.java
+++ app/src/main/java/com/project/scratchstudio/kith_andoid/Fragments/MessagesFragment.java
@@ -3,16 +3,36 @@ package com.project.scratchstudio.kith_andoid.Fragments;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.project.scratchstudio.kith_andoid.Activities.HomeActivity;
+import com.project.scratchstudio.kith_andoid.Adapters.MessageDialogAdapter;
+import com.project.scratchstudio.kith_andoid.Adapters.SearchAdapter;
+import com.project.scratchstudio.kith_andoid.Model.MessageDialogInfo;
+import com.project.scratchstudio.kith_andoid.Model.SearchInfo;
+import com.project.scratchstudio.kith_andoid.Model.User;
 import com.project.scratchstudio.kith_andoid.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessagesFragment extends Fragment {
+
+    private static long buttonCount = 0;
     private Bundle bundle;
+    private RecyclerView listView;
+    private MessageDialogAdapter adapter;
+
+    private static List<MessageDialogInfo> listMessages  = new ArrayList<>();
 
     public MessagesFragment() {}
 
@@ -30,9 +50,26 @@ public class MessagesFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         setButtonsListener();
+        listView = getActivity().findViewById(R.id.listMessages);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listView.setLayoutManager(llm);
 
+    }
+
+    public void setAdapter(){
+        adapter = new MessageDialogAdapter(getActivity(), listMessages, item -> {
+            if (SystemClock.elapsedRealtime() - buttonCount < 1000){
+                return;
+            }
+            buttonCount = SystemClock.elapsedRealtime();
+//            view.setEnabled(false);
+
+            //открытие диалога
+//            view.setEnabled(true);
+        });
+        listView.setAdapter(adapter);
     }
 
     private void setButtonsListener(){
